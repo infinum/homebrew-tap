429b1398631889aa7bdce5c7ca37627b8910d9e0
Jasmin Abou Aldan <jasmin.aboualdan@infinum.com>
2026-08-24 00:47:39 +0200

    Update app-deploy sha256 for retagged v2.2.0

--- Formula/app-deploy.rb
+++ Formula/app-deploy.rb
@@ -2,7 +2,7 @@ class AppDeploy < Formula
   desc "CLI for creating CI deployment trigger and build tags for mobile apps"
   homepage "https://github.com/infinum/app-deploy-script"
   url "https://github.com/infinum/app-deploy-script/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "b94451f98ff0df4f09e28a62588d9b9b4e4135131cb7494ece8db87a159848f7"
+  sha256 "e0d0a8b8c68477ab1845cb9d1fe688e0597237ab86efb06e25993eb916df5125"
   license "Apache-2.0"
 
   def install
